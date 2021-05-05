319ed08addc916b1bcebf42288dd7aae595c6815
Benny Jou <a33068843@gmail.com>
2021-05-05 17:33:44 +0800

    修正: 刪除不會用到的註解

--- src/scripts.js
+++ src/scripts.js
@@ -1,36 +1,3 @@
-// let students = ['詠晴', '品妍', '宥蓁', '柏睿', '宥廷', '柏宇'];
-// let studentsCheck = [];
-
-// for (let i = 0; i < students.length; i++) {
-//   let temp = Math.floor(Math.random() * students.length);
-
-//   while (studentsCheck[temp] === 1) {
-//     temp = Math.floor(Math.random() * students.length);
-//   }
-
-//   studentsCheck[temp] = 1;
-//   console.log(`${temp + 1} 號 ${students[temp]} 有沒有到?`);
-// }
-
-// let students = ['詠晴', '品妍', '宥蓁', '柏睿', '宥廷', '柏宇'];
-
-// // 產生隨機數字的 function
-// function getRandomInteger(minimum, maximum) {
-//   let min = Math.ceil(minimum);
-//   let max = Math.floor(maximum);
-//   return Math.floor(Math.random() * (max - min) + min);
-// }
-
-// // 先複製 students 陣列
-// let names = students.slice();
-
-// // 隨機唱名
-// while (names.length > 0) {
-//   let randomIndex = getRandomInteger(0, names.length);
-//   let randomName = names.splice(randomIndex, 1)[0];
-//   console.log(randomName);
-// }
-
 let targetword = document.getElementById('target');
 let now = '';
 let enword = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz !@#$%^&*.';
@@ -68,21 +35,3 @@ input.addEventListener('keypress', (e) => {
     run();
   }
 });
-
-// let word;
-// function getword() {
-//   let word = document.getElementById('typeValue').value;
-
-//   //targetword.innerHTML = word;
-//   console.log(word);
-// }
-
-// let form = document.querySelector('form');
-// form.addEventListener('submit', (event) => {
-//   event.preventDefault();
-// });
-
-// let input = document.querySelector('.iinput');
-//   input.addEventListener('click' () => {
-//   getword();
-// });
